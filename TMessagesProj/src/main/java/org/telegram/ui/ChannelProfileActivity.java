2a433f335c7c9c9d33cd0ca8fd2f3fc10f2ca88f
Harshitkant21 <harshitkant12@gmail.com>
2025-07-07 14:26:37 +0530

    Added test

--- TMessagesProj/src/main/java/org/telegram/ui/ChannelProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChannelProfileActivity.java
@@ -12,6 +12,7 @@ import org.telegram.messenger.NotificationCenter;
 import org.telegram.messenger.R;
 import org.telegram.messenger.UserConfig;
 import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.ActionBar.Theme;
 
 public class ChannelProfileActivity extends Activity{
 
